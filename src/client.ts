a3db6966aec1ed205bbeba9a63aca8dd5d0167b5
xiao cheng <xiaocheng@xiaos-MacBook-Air.local>
2020-11-27 22:52:14 -0600

    add client interceptor and deadline examples

--- src/client.ts
+++ src/client.ts
@@ -1,20 +1,56 @@
 import debug from "debug";
 import * as grpc from "@grpc/grpc-js";
+import _ from "lodash";
 
 import { Session, GetSessionRequest, GetAssignedSessionRequest, GetSessionsRequest } from "./proto/3dviz/session_pb";
 import { SessionServiceClient } from "./proto/3dviz/session_grpc_pb";
 
 const log = debug("SampleClient");
 const port: string | number = process.env.PORT || 50051;
-const client = new SessionServiceClient(`127.0.0.1:${port}`, grpc.credentials.createInsecure());
+
+// @ts-ignore
+const interceptor = (options: grpc.InterceptorOptions, nextCall) => {
+    let stored_listener;
+    let stored_metadata: grpc.Metadata;
+    return new grpc.InterceptingCall(nextCall(options), {
+        start: (metadata, listener, next) => {
+            //var span = apm.startSpan('grpc');
+            //var traceparent = span._context.toString();
+            //metadata.add('elastic-apm-traceparent', traceparent);
+            log("[client-interceptor]metadata:", metadata);
+            stored_listener = listener;
+            stored_metadata = metadata;
+            next(metadata, listener);
+            //span.end();
+        },
+        sendMessage: (message, next) => {
+            log("[client-interceptor]sendMessage:", message);
+            const options2 = _.clone(options);
+            const second_call = nextCall(options2);
+            second_call.start(stored_metadata);
+            second_call.sendMessage(message);
+            second_call.halfClose();
+            next(message);
+        },
+        halfClose: function (next) {
+            log("[client-interceptor]halfClose:");
+            next();
+        }
+    });
+}
+
+const client = new SessionServiceClient(`127.0.0.1:${port}`, grpc.credentials.createInsecure(),
+    { interceptors: [interceptor] });
 
 const getSession = async (id: string) => {
     return new Promise((resolve, reject) => {
         const request = new GetSessionRequest();
         request.setId(id);
         log(`[getSession] Request: ${JSON.stringify(request.toObject())}`);
-
-        client.getSession(request, (err, session: Session) => {
+        const metadata = new grpc.Metadata({ waitForReady: true });
+        const deadline = new Date();
+        deadline.setSeconds(deadline.getSeconds() + 10);
+        client.getSession(request, metadata, {deadline: deadline}, (err, session: Session) => {
             if (err != null) {
                 debug(`[getBook] err:\nerr.message: ${err.message}\nerr.stack:\n${err.stack}`);
                 reject(err);
@@ -68,9 +104,9 @@ const getSessions = () => {
 };
 
 async function main() {
-  //  await getSession('bogus');
- //   await getAssignedSession("xcheng4", "test");
-    await getSessions();
+    await getSession('bogus');
+    //   await getAssignedSession("xcheng4", "test");
+    //await getSessions();
 }
 
 main().then((_) => _);
